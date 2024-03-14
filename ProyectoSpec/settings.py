d9fd7d75db7c7c0d223b8648504afe24b63df9b0
Tomas Avendaño <tavendao@Gamasa.Local>
2024-03-14 16:08:36 -0300

    Tomex rrhh v3

--- ProyectoSpec/settings.py
+++ ProyectoSpec/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-==x8=_-rnul@%24(@81j1kh*5bk4gun95&5ze#ln&x%icyaep$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["10.55.6.42"]
+ALLOWED_HOSTS = ['10.55.6.42', '127.0.0.1', 'localhost', 'marcajes.gamasa.local']
 
 
 # Application definition
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/hector/proyecto/ProyectoG/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
